f8bd812b0c0e743245524da941aaad79f68ceff2
justinkk <justinkk@stanford.edu>
2015-05-07 11:09:14 -0700

    added everything hopefully

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public float speed;
+	public GameObject player;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Move towards the player
+		Vector2 direction = player.GetComponent<Transform>().position - GetComponent<Transform>().position;
+		direction = direction.normalized;
+		body.velocity = direction*speed;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float maxXSpeed;
+	public float maxYSpeed;
+	private Rigidbody2D body;
+
+	// Use this for initialization
+	void Start() {
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+		//Set the speed
+		Vector2 currSpeed = Vector2.zero;
+		currSpeed.x = Input.GetAxis("Horizontal")*maxXSpeed;
+		currSpeed.y = Input.GetAxis("Vertical")*maxYSpeed;
+
+		body.velocity = currSpeed;
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log("colliding");
+		
+		//Kill the player if the enemy eats you
+		if (coll.gameObject.tag == "Enemy")
+            Die();
+	}
+
+	void Die() {
+
+		Destroy(gameObject);
+	}
+}
