6c0b226d5cfe464f6d3f25df691c97bfed2f6216
justinkk <justinkk@stanford.edu>
2015-05-12 17:56:05 -0700

    the game exists now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,62 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
 	public float speed;
 	public GameObject player;
 	private Rigidbody2D body;
+	private AudioSource source;
+	private SpriteRenderer sprenderer;
+
+	public float minFreq;
+	public float maxFreq;
+	public float minDistance;
+	public float maxDistance;
+
+	private bool isHunting;
+
+	//private AudioSource source;
 
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D>();
+		source = GetComponent<AudioSource>();
+		sprenderer = GetComponent<SpriteRenderer>();
+
+		//Make the shark hidden and sneaky
+		sprenderer.enabled = false;
+
+		//Set position randomly, at least half the screen away
+		float newX = Random.Range(0.75f, 1.0f);
+		float newY = Random.Range(0.75f, 1.0f);
+		if (Random.value > 0.5)
+			newX = 1 - newX;
+		if (Random.value > 0.5)
+			newY = 1 - newY;
+		GetComponent<Transform>().position = Camera.main.ViewportToWorldPoint(new Vector3(newX, newY, 10)); //10 is to offset camera's z position
+
+		//Start making noise
+		isHunting = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		//Move towards the player
 		Vector2 direction = player.GetComponent<Transform>().position - GetComponent<Transform>().position;
-		direction = direction.normalized;
-		body.velocity = direction*speed;
+		Vector2 normalizedDirection = direction.normalized;
+		body.velocity = normalizedDirection*speed;
+
+		//Play sound with frequency dependant on distance
+					//Frequency scaled linearly     *     Proportion between minDistance and maxDistance, linearly
+		source.pitch = maxFreq - ((maxFreq - minFreq) * Mathf.Max((Mathf.Min(maxDistance, direction.magnitude) - minDistance)/(maxDistance - minDistance), 0f));
+		if(!source.isPlaying && isHunting) {
+			source.Play();
+		}
+	}
+
+	//Become visible and stop making noise
+	public void Eat() {
+		isHunting = false;
+		sprenderer.enabled = true;
 	}
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,92 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Player : MonoBehaviour {
-
 	public float maxXSpeed;
 	public float maxYSpeed;
 	private Rigidbody2D body;
+	private bool facingLeft = true;
+	private bool canMove = true;
+	private int score;
+
+	public float maxScale; //maximum x and y scale
+	public float growthRate; //what fraction of the gam to the maximum scale you grow with each seaweed
+
+
+	public Text scoreText;
+	public Text loseText;
+	public GameObject sharkObject;
+	private Enemy shark;
 
 	// Use this for initialization
 	void Start() {
 		body = GetComponent<Rigidbody2D>();
+
+		score = 0;
+
+		SetScore();
+		loseText.text = "";
+
+		shark = sharkObject.GetComponent<Enemy>();
 	}
 	
 	void FixedUpdate() {
-		//Set the speed
-		Vector2 currSpeed = Vector2.zero;
-		currSpeed.x = Input.GetAxis("Horizontal")*maxXSpeed;
-		currSpeed.y = Input.GetAxis("Vertical")*maxYSpeed;
+		//Stop after you die
+		if (canMove) {
+			//Set the speed
+			Vector2 currSpeed = Vector2.zero;
+			currSpeed.x = Input.GetAxis("Horizontal")*maxXSpeed;
+			currSpeed.y = Input.GetAxis("Vertical")*maxYSpeed;
 
-		body.velocity = currSpeed;
+			body.velocity = currSpeed;
+
+			//Flip if appropriate
+			if ((currSpeed.x < 0) != facingLeft && currSpeed.x != 0) {
+				facingLeft = !facingLeft;
+				Vector3 scale = GetComponent<Transform>().localScale;
+				scale.x *= -1;
+				GetComponent<Transform>().localScale = scale;
+			}
+		}	
 	}
 
-	void OnCollisionEnter2D(Collision2D coll) {
-		Debug.Log("colliding");
-		
+	void OnTriggerEnter2D(Collider2D other) {
+		GameObject otherObject = other.gameObject;
 		//Kill the player if the enemy eats you
-		if (coll.gameObject.tag == "Enemy")
+		if (otherObject.tag == "Enemy")
             Die();
+        else if (otherObject.tag == "Seaweed") {
+        	Destroy(otherObject);
+        	score++;
+        	SetScore();
+        	Grow();
+        }
 	}
 
 	void Die() {
+		loseText.text = "Game Over";
+		canMove = false;
+		body.velocity = Vector2.zero;
+
+		shark.Eat();
 
 		Destroy(gameObject);
+	}
+
+	void SetScore() {
+		scoreText.text = "Seaweed eaten: " + score;
+	}
+
+	void Grow() {
+		Vector3 currScale = transform.localScale;
+		float delta = ((maxScale - currScale.y) * growthRate);
+		
+		if (currScale.x > 0)
+			currScale.x += delta;
+		else
+			currScale.x -= delta;
+		currScale.y += delta;
+		transform.localScale = currScale;
 	}
 }
